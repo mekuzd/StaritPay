d766b7e4274320e6f23a0324e2548d67d810dcc0
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-26 22:12:04 +0100

    installed security dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,19 @@ const { PORT, MONGO_URI } = require("./src/Config/config");
 const Error404 = require("./src/middleware/error404");
 const taskRouter = require("./src/Routes/taskRouter");
 const app = express();
-app.use(express.json());
 
-//test route
+// security packages
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+
+//middleware
+app.use(cors({ origin: ["http://localhost:3030/"] }));
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(xss());
+
+// route
 app.get("/", (req, res) => {
   res.status(200).send("working");
 });
