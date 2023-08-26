509725f102e408d97e3566890dc699fd69a5d900
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-26 12:51:23 +0100

    creted test route and connected to  MONGODB

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { PORT, MONGO_URI } = require("./src/Config/config");
+
+const app = express();
+
+//test route
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+// connect to db and listen on PORT
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
