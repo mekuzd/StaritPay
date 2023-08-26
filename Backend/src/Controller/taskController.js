617abd8cf266036f2daef4c5d3ecd64a4ac10276
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-26 21:48:04 +0100

    created new route for updating task,deleting task and get all task

--- Backend/src/Controller/taskController.js
+++ Backend/src/Controller/taskController.js
@@ -8,6 +8,49 @@ const createTask = async (req, res) => {
     res.status(500).json({ message: "please contact your admin" });
   }
 };
+
+const updateTask = async (req, res) => {
+  try {
+    const taskExist = await Task.findOne({ _id: req.params.id });
+    if (!taskExist) {
+      return res
+        .status(404)
+        .json({ message: `No task with id : ${req.params.id}` });
+    }
+    const task = await Task.findOneAndUpdate({ _id: taskExist._id }, req.body, {
+      new: true,
+    });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: "please contact your admin" });
+  }
+};
+const deleteTask = async (req, res) => {
+  try {
+    const taskExist = await Task.findOne({ _id: req.params.id });
+    if (!taskExist) {
+      return res
+        .status(404)
+        .json({ message: `No task with id : ${req.params.id}` });
+    }
+    const task = await Task.findOneAndRemove({ _id: taskExist._id });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: "please contact your admin" });
+  }
+};
+
+const getAlltask = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ message: "please contact your admin" });
+  }
+};
 module.exports = {
   createTask,
+  updateTask,
+  deleteTask,
+  getAlltask,
 };
--- Backend/src/Routes/taskRouter.js
+++ Backend/src/Routes/taskRouter.js
@@ -1,6 +1,14 @@
 const { Router } = require("express");
-const { createTask } = require("../Controller/taskController");
+const {
+  createTask,
+  getAlltask,
+  updateTask,
+  deleteTask,
+} = require("../Controller/taskController");
 
 const router = Router();
+router.get("/", getAlltask);
 router.post("/", createTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
 module.exports = router;
