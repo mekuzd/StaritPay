1bd788c12ecd97e2a5e5033abf19fd5700c9fcc6
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-27 22:33:33 +0100

    installed bcryptjs created usermodel and usercontroller file

---
+++ Backend/src/Controller/userController.js
@@ -0,0 +1,19 @@
+const User = require("../Model/user");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    return res.status(403).json({ message: "user already registered" });
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
