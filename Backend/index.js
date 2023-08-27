940e77d0b038c91cb0900a71daedd6640e085f33
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-27 15:26:20 +0100

    added https://strait-pay.vercel.app/ to cors in index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,11 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 
 //middleware
-app.use(cors({ origin: ["http://localhost:3030"] }));
+app.use(
+  cors({
+    origin: ["http://localhost:3030", "https://strait-pay.vercel.app"],
+  }),
+);
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(xss());
